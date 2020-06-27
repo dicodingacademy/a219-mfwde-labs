c773051d16285381d2dd3ee99b6a9ee51b00ef79
Dimas <dimas.maulanads@gmail.com>
2020-06-27 20:22:17 +0700

    Adding functional to screen off canvas

--- dicoding-blog/src/index.js
+++ dicoding-blog/src/index.js
@@ -1,2 +1,21 @@
 import './styles/main.css';
 import './styles/responsive.css';
+
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function (event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
