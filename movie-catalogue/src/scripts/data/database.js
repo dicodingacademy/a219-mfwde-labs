d126969ac887ec2b0206488f915f354b8a0b0fdb
Dimas <dimas.maulanads@gmail.com>
2020-06-27 21:52:13 +0700

    Creating database operation helper

---
+++ movie-catalogue/src/scripts/data/database.js
@@ -0,0 +1,27 @@
+import { openDB } from 'idb';
+import CONFIG from '../globals/config';
+
+const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
+
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade(database) {
+    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
+  },
+});
+
+const database = {
+  async getMovie(id) {
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+  async getAllMovies() {
+    return (await dbPromise).getAll(OBJECT_STORE_NAME);
+  },
+  async putMovie(movie) {
+    return (await dbPromise).put(OBJECT_STORE_NAME, movie);
+  },
+  async deleteMovie(id) {
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+  },
+};
+
+export default database;
--- movie-catalogue/src/scripts/globals/config.js
+++ movie-catalogue/src/scripts/globals/config.js
@@ -4,6 +4,9 @@ const CONFIG = {
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
 };
 
 export default CONFIG;
