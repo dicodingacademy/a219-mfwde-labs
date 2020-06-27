08fa00ff713aaed51e7a30a8d13d27b7e1b0725a
Dimas <dimas.maulanads@gmail.com>
2020-06-27 20:48:50 +0700

    Make functionality for drawer

--- movie-catalogue/src/scripts/index.js
+++ movie-catalogue/src/scripts/index.js
@@ -1,5 +1,10 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import App from './views/app';
 
-console.log('Hello Coders!');
+const app = new App({
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
+});
---
+++ movie-catalogue/src/scripts/views/app.js
@@ -0,0 +1,23 @@
+import DrawerInitiator from '../utils/drawer-initiator';
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+
+    // kita bisa menginisiasikan komponen lain bila ada
+  }
+}
+
+export default App;
