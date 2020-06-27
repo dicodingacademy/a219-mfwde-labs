0f810fd769da0891e5ad20c95d5f38c1d0f3b223
Dimas <dimas.maulanads@gmail.com>
2020-06-27 22:05:33 +0700

    Adding notification

---
+++ movie-catalogue/src/scripts/utils/notification-helper.js
@@ -0,0 +1,43 @@
+const NotificationHelper = {
+  sendNotification({ title, options }) {
+    if (!this._checkAvailability()) {
+      console.log('Notification not supported in this browser');
+      return;
+    }
+
+    if (!this._checkPermission()) {
+      console.log('User did not yet granted permission');
+      this._requestPermission();
+      return;
+    }
+
+    this._showNotification({ title, options });
+  },
+
+  _checkAvailability() {
+    return !!('Notification' in window);
+  },
+
+  _checkPermission() {
+    return Notification.permission === 'granted';
+  },
+
+  async _requestPermission() {
+    const status = await Notification.requestPermission();
+
+    if (status === 'denied') {
+      console.log('Notification Denied');
+    }
+
+    if (status === 'default') {
+      console.log('Permission closed');
+    }
+  },
+
+  async _showNotification({ title, options }) {
+    const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+    serviceWorkerRegistration.showNotification(title, options);
+  },
+};
+
+export default NotificationHelper;
--- movie-catalogue/src/scripts/utils/websocket-initiator.js
+++ movie-catalogue/src/scripts/utils/websocket-initiator.js
@@ -1,12 +1,22 @@
-const WebsocketInitiator = {
+import NotificationHelper from './notification-helper';
+import CONFIG from '../globals/config';
+
+const WebSocketInitiator = {
   init(url) {
     const webSocket = new WebSocket(url);
     webSocket.onmessage = this._onMessageHandler;
   },
 
   _onMessageHandler(message) {
-    console.log(message.data);
+    const movie = JSON.parse(message.data);
+    NotificationHelper.sendNotification({
+      title: `${movie.title} is on cinema!`,
+      options: {
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
+    });
   },
 };
 
-export default WebsocketInitiator;
+export default WebSocketInitiator;
