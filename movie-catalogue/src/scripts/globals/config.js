549cc01fb5128f8dc10ba84b82895085411238a1
Dimas <dimas.maulanads@gmail.com>
2020-06-27 21:48:44 +0700

    Create cache helper

--- movie-catalogue/src/scripts/globals/config.js
+++ movie-catalogue/src/scripts/globals/config.js
@@ -3,6 +3,7 @@ const CONFIG = {
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
 };
 
 export default CONFIG;
