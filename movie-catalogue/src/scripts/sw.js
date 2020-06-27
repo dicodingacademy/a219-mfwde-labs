24a7f291c0747638f68def9bb8a63fa3ee9b3fd8
Dimas <dimas.maulanads@gmail.com>
2020-06-27 21:49:55 +0700

    Applying cache to Service Worker to offline functionality

--- movie-catalogue/src/scripts/sw.js
+++ movie-catalogue/src/scripts/sw.js
@@ -1,19 +1,16 @@
-self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...');
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
 
-  // TODO: Caching App Shell Resource
+const { assets } = global.serviceWorkerOption;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-
-  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
