693d84f1d2584a8e42bf09104f8b83b3ff88d388
Dimas <dimas.maulanads@gmail.com>
2020-06-27 22:03:35 +0700

    Adding websocket

--- movie-catalogue/src/scripts/index.js
+++ movie-catalogue/src/scripts/index.js
@@ -3,6 +3,8 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -17,4 +19,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
   app.renderPage();
   swRegister();
+  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
---
+++ movie-catalogue/src/scripts/utils/websocket-initiator.js
@@ -0,0 +1,12 @@
+const WebsocketInitiator = {
+  init(url) {
+    const webSocket = new WebSocket(url);
+    webSocket.onmessage = this._onMessageHandler;
+  },
+
+  _onMessageHandler(message) {
+    console.log(message.data);
+  },
+};
+
+export default WebsocketInitiator;
