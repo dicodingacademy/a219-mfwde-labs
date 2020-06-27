6b69009dd93ba0e65114f68d85841edd98d3549c
Dimas <dimas.maulanads@gmail.com>
2020-06-27 21:59:32 +0700

    Showing liked movies

--- movie-catalogue/src/scripts/routes/routes.js
+++ movie-catalogue/src/scripts/routes/routes.js
@@ -1,12 +1,14 @@
 import NowPlaying from '../views/pages/now-playing';
 import Upcoming from '../views/pages/upcoming';
 import Detail from '../views/pages/detail';
+import Like from '../views/pages/like';
 
 const routes = {
   '/': NowPlaying, // default page
   '/now-playing': NowPlaying,
   '/upcoming': Upcoming,
   '/detail/:id': Detail,
+  '/like': Like,
 };
 
 export default routes;
---
+++ movie-catalogue/src/scripts/views/pages/like.js
@@ -0,0 +1,25 @@
+import database from '../../data/database';
+import { createMovieItemTemplate } from '../templates/template-creator';
+
+const Like = {
+  async render() {
+    return `
+      <div class="content">
+        <h2 class="content__heading">Now Playing in Cinema</h2>
+        <div id="movies" class="movies">
+
+        </div>
+      </div>
+    `;
+  },
+
+  async afterRender() {
+    const movies = await database.getAllMovies();
+    const moviesContainer = document.querySelector('#movies');
+    movies.forEach((movie) => {
+      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    });
+  },
+};
+
+export default Like;
