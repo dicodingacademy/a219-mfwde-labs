5cc7ffac30d25b96c9c903b9a840aa73e4eef1f8
Dimas <dimas.maulanads@gmail.com>
2020-07-04 01:29:05 +0700

    Cat-alog initial

---
+++ cat-alog/webpack.common.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/images/'),
+          to: path.resolve(__dirname, 'dist/images/'),
+        },
+      ],
+    }),
+  ],
+};
---
+++ cat-alog/webpack.dev.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+});
