7f850f762777c421eb83c63856ff2957562a20cf
Dimas <dimas.maulanads@gmail.com>
2020-07-06 16:03:03 +0700

    Migrate to express

---
+++ cat-alog/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('dist'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', (request, response) => {
+  response.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`);
+});
--- cat-alog/webpack.config.js
+++ cat-alog/webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
+  mode: 'production',
   output: {
     filename: 'bundle.js',
   },
