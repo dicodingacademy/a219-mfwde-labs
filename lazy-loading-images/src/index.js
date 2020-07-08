ee0e3ddf99fbf33a0c62542c01f3c9c7d23570e8
Dimas <dimas.maulanads@gmail.com>
2020-07-08 11:00:44 +0700

    Lazy loading using Lazysizes

--- lazy-loading-images/src/index.js
+++ lazy-loading-images/src/index.js
@@ -1,9 +1,12 @@
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
 const START = 10;
 const NUMBER_OF_IMAGES = 100;
 
 const generateImage = () => {
   for (let i = START; i < START + NUMBER_OF_IMAGES; i++) {
-    document.body.innerHTML += `<img loading="lazy" src="https://picsum.photos/id/${i}/400/400" width="400" height="400" alt="dummy images"><br>`;
+    document.body.innerHTML += `<img class="lazyload" data-src="https://picsum.photos/id/${i}/400/400" alt="dummy images"><br>`;
   }
 };
 
