edc3e857fb9f47b8d85a9ce5032e5bc90a5ab937
Dimas <dimas.maulanads@gmail.com>
2020-07-08 10:45:13 +0700

    Lazy loading native ways

--- lazy-loading-images/src/index.js
+++ lazy-loading-images/src/index.js
@@ -3,7 +3,7 @@ const NUMBER_OF_IMAGES = 100;
 
 const generateImage = () => {
   for (let i = START; i < START + NUMBER_OF_IMAGES; i++) {
-    document.body.innerHTML += `<img src="https://picsum.photos/id/${i}/400/400" alt="dummy images"><br>`;
+    document.body.innerHTML += `<img loading="lazy" src="https://picsum.photos/id/${i}/400/400" width="400" height="400" alt="dummy images"><br>`;
   }
 };
 
