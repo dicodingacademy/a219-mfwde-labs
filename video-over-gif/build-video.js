59113e38268da1000eb7fd179ddd6bf758e90315
Dimas <dimas.maulanads@gmail.com>
2020-07-14 13:32:51 +0700

    Update video over gif solution

---
+++ video-over-gif/build-video.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const FFmpeg = require('fluent-ffmpeg');
+
+const gify = new FFmpeg({ source: path.resolve(__dirname, 'src/images/animation-footage.gif') });
+
+gify.clone()
+  .withVideoCodec('libx264')
+  .withFps(25)
+  .toFormat('mp4')
+  .saveToFile(path.resolve(__dirname, 'src/images/animation-footage.mp4'));
+
+gify.clone()
+  .withFps(25)
+  .toFormat('webm')
+  .saveToFile(path.resolve(__dirname, 'src/images/animation-footage.webm'));
