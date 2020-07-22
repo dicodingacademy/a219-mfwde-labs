b5f2040fa15801200408cf8be561a48713957871
Dimas <dimas.maulanads@gmail.com>
2020-07-22 16:59:28 +0700

    Lazyload Font Awesome

--- movie-catalogue/src/scripts/views/pages/detail.js
+++ movie-catalogue/src/scripts/views/pages/detail.js
@@ -12,6 +12,15 @@ const Detail = {
   },
 
   async afterRender() {
+    // lazy load font awesome
+    let scriptElement = document.querySelector('script[src="https://use.fontawesome.com/b070c8f1df.js"]');
+
+    if (!scriptElement) {
+      scriptElement = document.createElement('script');
+      scriptElement.src = 'https://use.fontawesome.com/b070c8f1df.js';
+      document.body.appendChild(scriptElement);
+    }
+
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const movie = await TheMovieDbSource.detailMovie(url.id);
     const movieContainer = document.querySelector('#movie');
