13bb175d307ba86fba8b41e5cfc7486370acadea
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-07-29 22:00:39 +0700

    Jangan Bertele-tele

--- movie-catalogue/specs/favoriteMovieSearchSpec.js
+++ movie-catalogue/specs/favoriteMovieSearchSpec.js
@@ -3,7 +3,15 @@ import FavoriteMovieSearchPresenter
 import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
 
 describe('Searching movies', () => {
-  beforeEach(() => {
+  let presenter;
+
+  const searchMovies = (query) => {
+    const queryElement = document.getElementById('query');
+    queryElement.value = query;
+    queryElement.dispatchEvent(new Event('change'));
+  };
+
+  const setMovieSearchContainer = () => {
     document.body.innerHTML = `
         <div id="movie-search-container">
             <input id="query" type="text">
@@ -13,30 +21,65 @@ describe('Searching movies', () => {
             </div>
         </div>
         `;
+  };
+
+  const constructPresenter = () => {
+    spyOn(FavoriteMovieIdb, 'searchMovies');
+    presenter = new FavoriteMovieSearchPresenter({
+      favoriteMovies: FavoriteMovieIdb,
+    });
+  };
+
+  beforeEach(() => {
+    setMovieSearchContainer();
+    constructPresenter();
   });
 
   it('should be able to capture the query typed by the user', () => {
-    spyOn(FavoriteMovieIdb, 'searchMovies');
-    const presenter = new FavoriteMovieSearchPresenter({
-      favoriteMovies: FavoriteMovieIdb,
-    });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'film a';
-    queryElement.dispatchEvent(new Event('change'));
+    searchMovies('film a');
 
     expect(presenter.latestQuery).toEqual('film a');
   });
 
-  it('should ask the model to search for liked movies', () => {
-    spyOn(FavoriteMovieIdb, 'searchMovies');
-    const presenter = new FavoriteMovieSearchPresenter({ favoriteMovies: FavoriteMovieIdb });
-
-    const queryElement = document.getElementById('query');
-    queryElement.value = 'film a';
-    queryElement.dispatchEvent(new Event('change'));
+  it('should ask the model to search for movies', () => {
+    searchMovies('film a');
 
     expect(FavoriteMovieIdb.searchMovies)
       .toHaveBeenCalledWith('film a');
+  });
+
+  it('should show the found movies', () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+    expect(document.querySelectorAll('.movie').length).toEqual(1);
+
+    presenter._showFoundMovies([{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }]);
+    expect(document.querySelectorAll('.movie').length).toEqual(2);
+  });
+
+  it('should show the title of the found movies', () => {
+    presenter._showFoundMovies([{ id: 1, title: 'Satu' }]);
+    expect(document.querySelectorAll('.movie__title').item(0).textContent)
+      .toEqual('Satu');
+  });
+
+  it('should show the title of the found movies', () => {
+    presenter._showFoundMovies([{ id: 1, title: 'Satu' }]);
+    expect(document.querySelectorAll('.movie__title').item(0).textContent)
+      .toEqual('Satu');
+
+    presenter._showFoundMovies(
+      [{ id: 1, title: 'Satu' }, { id: 2, title: 'Dua' }],
+    );
+
+    const movieTitles = document.querySelectorAll('.movie__title');
+    expect(movieTitles.item(0).textContent).toEqual('Satu');
+    expect(movieTitles.item(1).textContent).toEqual('Dua');
+  });
+
+  it('should show - for found movie without title', () => {
+    presenter._showFoundMovies([{ id: 1 }]);
+
+    expect(document.querySelectorAll('.movie__title').item(0).textContent)
+      .toEqual('-');
   });
 });
--- movie-catalogue/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ movie-catalogue/src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -7,9 +7,22 @@ class FavoriteMovieSearchPresenter {
   _listenToSearchRequestByUser() {
     this._queryElement = document.getElementById('query');
     this._queryElement.addEventListener('change', (event) => {
-      this._latestQuery = event.target.value;
-      this._favoriteMovies.searchMovies(this._latestQuery);
+      this._searchMovies(event.target.value);
     });
+  }
+
+  _searchMovies(latestQuery) {
+    this._latestQuery = latestQuery;
+    this._favoriteMovies.searchMovies(this.latestQuery);
+  }
+
+  _showFoundMovies(movies) {
+    const html = movies.reduce(
+      (carry, movie) => carry.concat(`<li class="movie"><span class="movie__title">${movie.title || '-'}</span></li>`),
+      '',
+    );
+
+    document.querySelector('.movies').innerHTML = html;
   }
 
   get latestQuery() {
