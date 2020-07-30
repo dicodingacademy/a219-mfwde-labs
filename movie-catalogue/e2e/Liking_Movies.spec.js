79fabfe2b6d97f0ce48dd75ed4fa5718d39eafcc
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-07-30 22:56:34 +0700

    Membekukan waktu

--- movie-catalogue/e2e/Liking_Movies.spec.js
+++ movie-catalogue/e2e/Liking_Movies.spec.js
@@ -16,3 +16,18 @@ Scenario('liking one movie', (I) => {
   I.amOnPage('/');
   // … kita akan mengisi uji coba berikutnya …
 });
+
+Scenario('liking one movie', (I) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.movie__title a');
+  I.click(locate('.movie__title a').first());
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/like');
+  I.seeElement('.movie-item');
+});
