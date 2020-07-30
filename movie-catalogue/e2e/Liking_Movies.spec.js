a27b66f920bfe9bafc7657397365bd22184ab6aa
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-07-30 23:09:50 +0700

    Jangan sampai salah nama

--- movie-catalogue/e2e/Liking_Movies.spec.js
+++ movie-catalogue/e2e/Liking_Movies.spec.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 Feature('Liking Movies');
 
 Before((I) => {
@@ -10,24 +12,23 @@ Scenario('showing empty liked movies', (I) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 });
 
-Scenario('liking one movie', (I) => {
-  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
-
-  I.amOnPage('/');
-  // … kita akan mengisi uji coba berikutnya …
-});
-
-Scenario('liking one movie', (I) => {
+Scenario('liking one movie', async (I) => {
   I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
 
   I.amOnPage('/');
 
   I.seeElement('.movie__title a');
-  I.click(locate('.movie__title a').first());
+
+  const firstFilm = locate('.movie__title a').first();
+  const firstFilmTitle = await I.grabTextFrom(firstFilm);
+  I.click(firstFilm);
 
   I.seeElement('#likeButton');
   I.click('#likeButton');
 
   I.amOnPage('/#/like');
   I.seeElement('.movie-item');
+  const likedFilmTitle = await I.grabTextFrom('.movie__title');
+
+  assert.strictEqual(firstFilmTitle, likedFilmTitle);
 });
