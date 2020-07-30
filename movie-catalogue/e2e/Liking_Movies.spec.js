d5187a6418935e8f6bbd7ca62a8760bcc43706b3
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-07-30 23:24:25 +0700

    Sebuah pencarian

--- movie-catalogue/e2e/Liking_Movies.spec.js
+++ movie-catalogue/e2e/Liking_Movies.spec.js
@@ -32,3 +32,38 @@ Scenario('liking one movie', async (I) => {
 
   assert.strictEqual(firstFilmTitle, likedFilmTitle);
 });
+
+Scenario('searching movies', async (I) => {
+  I.see('Tidak ada film untuk ditampilkan', '.movie-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.movie__title a');
+
+  const titles = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.movie__title a').at(i));
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+    titles.push(await I.grabTextFrom('.movie__title'));
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/like');
+  I.seeElement('#query');
+
+  const searchQuery = titles[1].substring(1, 3);
+  const matchingMovies = titles.filter((title) => title.indexOf(searchQuery) !== -1);
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const visibleLikedMovies = await I.grabNumberOfVisibleElements('.movie-item');
+  assert.strictEqual(matchingMovies.length, visibleLikedMovies);
+
+  matchingMovies.forEach(async (title, index) => {
+    const visibleTitle = await I.grabTextFrom(locate('.movie__title').at(index + 1));
+    assert.strictEqual(title, visibleTitle);
+  });
+});
