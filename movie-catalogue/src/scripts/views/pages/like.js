b3686d64a01155c94998b3ac52c2b24ba77f1279
Dimas Maulana Dwi Saputra <dimas.maulanads@gmail.com>
2020-07-30 22:11:27 +0700

    End-to-End Testing

--- movie-catalogue/src/scripts/views/pages/like.js
+++ movie-catalogue/src/scripts/views/pages/like.js
@@ -1,24 +1,18 @@
 import FavoriteMovieIdb from '../../data/favorite-movie-idb';
-import { createMovieItemTemplate } from '../templates/template-creator';
+import FavoriteMovieSearchView from './liked-movies/favorite-movie-search-view';
+import FavoriteMovieShowPresenter from './liked-movies/favorite-movie-show-presenter';
+import FavoriteMovieSearchPresenter from './liked-movies/favorite-movie-search-presenter';
+
+const view = new FavoriteMovieSearchView();
 
 const Like = {
   async render() {
-    return `
-      <div class="content">
-        <h2 class="content__heading">Your Liked Movie</h2>
-        <div id="movies" class="movies">
-
-        </div>
-      </div>
-    `;
+    return view.getTemplate();
   },
 
   async afterRender() {
-    const movies = await FavoriteMovieIdb.getAllMovies();
-    const moviesContainer = document.querySelector('#movies');
-    movies.forEach((movie) => {
-      moviesContainer.innerHTML += createMovieItemTemplate(movie);
-    });
+    new FavoriteMovieShowPresenter({ view, favoriteMovies: FavoriteMovieIdb });
+    new FavoriteMovieSearchPresenter({ view, favoriteMovies: FavoriteMovieIdb });
   },
 };
 
