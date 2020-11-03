90ba40ef3fe39ac6fd1533faf841386721af0aca
Dimas Saputra <dimas@dicoding.com>
2020-11-03 13:58:57 +0700

    change web socket server url

--- movie-catalogue/src/scripts/globals/config.js
+++ movie-catalogue/src/scripts/globals/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   DATABASE_NAME: 'movie-catalogue-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'movies',
-  WEB_SOCKET_SERVER: 'wss://stormy-badlands-06326.herokuapp.com',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
 };
 
 export default CONFIG;
