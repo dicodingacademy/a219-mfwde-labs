72f35f9a1d47bcfa3e33e383e9c8b2771800bbaa
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-06-22 21:53:37 +0700

    Update node package version
    add gitignore file, update package version, etc.

--- cat-alog/webpack.config.js
+++ cat-alog/webpack.config.js
@@ -1,14 +1,17 @@
-const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
-const ImageminMozjpeg = require('imagemin-mozjpeg');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
+const ImageminMozjpeg = require("imagemin-mozjpeg");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
-  mode: 'production',
+  mode: "production",
+  entry: {
+    app: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
@@ -16,10 +19,10 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader',
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
           },
         ],
       },
@@ -27,14 +30,14 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/index.html'),
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/index.html"),
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/images/'),
-          to: path.resolve(__dirname, 'dist/images/'),
+          from: path.resolve(__dirname, "src/images/"),
+          to: path.resolve(__dirname, "dist/images/"),
         },
       ],
     }),
