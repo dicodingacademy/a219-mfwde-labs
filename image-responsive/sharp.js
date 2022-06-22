cb31a666069fd244015713f00cb096bacf6ba2b7
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-06-22 22:04:14 +0700

    Update node package version
    Reformat code, update node package version, etc.

--- image-responsive/sharp.js
+++ image-responsive/sharp.js
@@ -10,17 +10,20 @@ if (!fs.existsSync(destination)) {
 }
 
 fs.readdirSync(target)
-  .forEach(image => {
+    .forEach(image => {
+      // mengubah ukuran gambar dengan lebar 800px, dengan prefix -large.jpg
+      sharp(`${target}/${image}`)
+          .resize(800)
+          .toFile(path.resolve(
+              __dirname,
+              `${destination}/${image.split('.').slice(0, -1).join('.')}-large.jpg`),
+          );
 
-    sharp(`${target}/${image}`)
-      .resize(800)
-      .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
-        .slice(0, -1)
-        .join('.')}-large.jpg`));
-
-    sharp(`${target}/${image}`)
-      .resize(480)
-      .toFile(path.resolve(__dirname, `${destination}/${image.split('.')
-        .slice(0, -1)
-        .join('.')}-small.jpg`));
-  });
+      // mengubah ukuran gambar dengan lebar 480px, dengan prefix -small.jpg
+      sharp(`${target}/${image}`)
+          .resize(480)
+          .toFile(path.resolve(
+              __dirname,
+              `${destination}/${image.split('.').slice(0, -1).join('.')}-small.jpg`),
+          );
+    });
--- image-responsive/webpack.config.js
+++ image-responsive/webpack.config.js
@@ -1,13 +1,15 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, 'src/index.js'),
+  mode: "production",
+  entry: {
+    app: path.resolve(__dirname, "src/index.js"),
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
@@ -15,10 +17,10 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader",
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
           },
         ],
       },
@@ -26,16 +28,17 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: path.resolve(__dirname, 'src/templates/index.html')
+      filename: "index.html",
+      template: path.resolve(__dirname, "src/templates/index.html"),
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public'),
-          to: path.resolve(__dirname, 'dist'),
+          from: path.resolve(__dirname, "src/public"),
+          to: path.resolve(__dirname, "dist"),
           globOptions: {
-            ignore: ['**/images/**'],
+            // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
+            ignore: ["**/images/**"],
           },
         },
       ],
