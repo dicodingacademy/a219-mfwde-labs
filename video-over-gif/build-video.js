a48f478ddcc3caed4dec54f2aca1cc739a980bae
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-06-22 22:00:18 +0700

    Reformat Code

--- video-over-gif/build-video.js
+++ video-over-gif/build-video.js
@@ -1,15 +1,19 @@
-const path = require('path');
-const FFmpeg = require('fluent-ffmpeg');
+const path = require("path");
+const FFmpeg = require("fluent-ffmpeg");
 
-const gify = new FFmpeg({ source: path.resolve(__dirname, 'src/images/animation-footage.gif') });
+const gify = new FFmpeg({
+  source: path.resolve(__dirname, "src/images/animation-footage.gif"),
+});
 
-gify.clone()
-  .withVideoCodec('libx264')
+gify
+  .clone()
+  .withVideoCodec("libx264")
   .withFps(25)
-  .toFormat('mp4')
-  .saveToFile(path.resolve(__dirname, 'src/images/animation-footage.mp4'));
+  .toFormat("mp4")
+  .saveToFile(path.resolve(__dirname, "src/images/animation-footage.mp4"));
 
-gify.clone()
+gify
+  .clone()
   .withFps(25)
-  .toFormat('webm')
-  .saveToFile(path.resolve(__dirname, 'src/images/animation-footage.webm'));
+  .toFormat("webm")
+  .saveToFile(path.resolve(__dirname, "src/images/animation-footage.webm"));
