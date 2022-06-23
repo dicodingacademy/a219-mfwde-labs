43c00d95694c73dc2a5d20a461875a871841d2ac
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-06-23 14:08:24 +0700

    Update node package version
    Update node package version, reformat code, add gitignore, etc.

--- dicoding-blog/webpack.config.js
+++ dicoding-blog/webpack.config.js
@@ -2,46 +2,54 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: path.resolve(__dirname, 'dist/index.html'),
-            template: path.resolve(__dirname, 'src/index.html'),
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/images'),
-                    to: path.resolve(__dirname, 'dist/images'),
-                },
-            ],
-        }),
-        new FaviconsWebpackPlugin({
-            logo: path.resolve(__dirname, 'src/images/dicoding.jpeg'),
-        }),
-        new CleanWebpackPlugin(),
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/images'),
+          to: path.resolve(__dirname, 'dist/images'),
+        },
+      ],
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, 'src/images/dicoding.jpeg'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
