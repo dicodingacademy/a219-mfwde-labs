7df9a7f8dc38b1df9d79f1cf208915e3d964851a
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-06-24 16:07:33 +0700

    Update node package version
    Add .gitignore, add extra webpack config, reformat code, etc.

--- movie-catalogue/webpack.prod.js
+++ movie-catalogue/webpack.prod.js
@@ -1,13 +1,14 @@
-const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: '/node_modules/',
+        exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
