17fb6f60b94a3f3ed7453236d1ffba31b87758d0
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-06-24 10:17:57 +0700

    Update node package version
    Reformat code, update package, remove service worker webpack plugin, update registering service worker, add gitignore, give extra webpack dev config. etc.

--- movie-catalogue/src/scripts/utils/sw-register.js
+++ movie-catalogue/src/scripts/utils/sw-register.js
@@ -1,11 +1,16 @@
-import runtime from 'serviceworker-webpack-plugin/lib/runtime';
-
 const swRegister = async () => {
-  if ('serviceWorker' in navigator) {
-    await runtime.register();
+  if (!('serviceWorker' in navigator)) {
+    console.log('Service Worker not supported in the browser');
     return;
   }
-  console.log('Service worker not supported in this browser');
+
+  try {
+    await navigator.serviceWorker.register('./sw.bundle.js');
+    console.log('Service worker registered');
+  } catch (error) {
+    console.log('Failed to register service worker', error);
+  }
 };
 
 export default swRegister;
+
--- movie-catalogue/webpack.dev.js
+++ movie-catalogue/webpack.dev.js
@@ -4,7 +4,17 @@ const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    port: 9000,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    compress: true,
   },
 });
