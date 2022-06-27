188db294868297ec0f3f318003a111ec64e4fc60
Nur Rizki Adi Prasetyo <66776057+nurrizkiadip@users.noreply.github.com>
2022-06-27 10:56:20 +0700

    Reconfigure service worker file

--- movie-catalogue/src/scripts/sw.js
+++ movie-catalogue/src/scripts/sw.js
@@ -1,10 +1,26 @@
 import 'regenerator-runtime';
 import CacheHelper from './utils/cache-helper';
 
-const { assets } = global.serviceWorkerOption;
+// Caching the listed asset below
+const assetsToCache = [
+  './',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
