871f896fa0fb701ccc596f13dcc8a3f925ff073d
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-08-02 12:39:31 +0700

    Add workbox webpack plugin and remove sw.js

--- movie-catalogue/src/scripts/utils/sw-register.js
+++ movie-catalogue/src/scripts/utils/sw-register.js
@@ -1,11 +1,14 @@
+import * as WorkboxWindow from 'workbox-window';
+
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
     console.log('Service Worker not supported in the browser');
     return;
   }
 
+  const wb = new WorkboxWindow.Workbox('/sw.bundle.js');
   try {
-    await navigator.serviceWorker.register('./sw.bundle.js');
+    await wb.register();
     console.log('Service worker registered');
   } catch (error) {
     console.log('Failed to register service worker', error);
--- movie-catalogue/webpack.common.js
+++ movie-catalogue/webpack.common.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
-    sw: path.resolve(__dirname, 'src/scripts/sw.js'),
   },
   output: {
     filename: '[name].bundle.js',
@@ -39,6 +39,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
     }),
   ],
 };
