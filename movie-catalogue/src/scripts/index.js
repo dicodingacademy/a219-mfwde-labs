50b5dbe278f72acdb9a2a4f99a2262c296c92744
Nur Rizki Adi Prasetyo <nrap.rizki10@gmail.com>
2022-08-18 17:03:13 +0700

    Add web push notification feature

--- movie-catalogue/src/scripts/index.js
+++ movie-catalogue/src/scripts/index.js
@@ -4,6 +4,7 @@ import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
 import WebSocketInitiator from './utils/websocket-initiator';
+import FooterToolsInitiator from './utils/footer-tools-initiator';
 import CONFIG from './globals/config';
 
 const app = new App({
@@ -16,8 +17,14 @@ window.addEventListener('hashchange', () => {
   app.renderPage();
 });
 
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
   app.renderPage();
-  swRegister();
+  await swRegister();
   WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
+
+  // Initialize footer tools
+  FooterToolsInitiator.init({
+    subscribeButton: document.querySelector('#subscribePushNotification'),
+    unsubscribeButton: document.querySelector('#unsubscribePushNotification'),
+  });
 });
