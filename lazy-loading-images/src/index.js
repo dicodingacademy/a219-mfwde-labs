2de1e100b8c480e49d43c172cc4281a6e80be374
Nur Rizki Adi Prasetyo <nrizki@dicoding.com>
2023-10-06 07:36:19 +0700

    Update libraries versions

--- lazy-loading-images/src/index.js
+++ lazy-loading-images/src/index.js
@@ -4,6 +4,10 @@ import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 const START = 10;
 const NUMBER_OF_IMAGES = 100;
 
+/**
+ * Full documentations:
+ * https://github.com/aFarkas/lazysizes#more-about-the-api
+ */
 const generateImage = () => {
   for (let i = START; i < START + NUMBER_OF_IMAGES; i++) {
     document.body.innerHTML += `<img class="lazyload" data-src="https://picsum.photos/id/${i}/400/400" alt="dummy images"><br>`;
--- lazy-loading-images/webpack.config.js
+++ lazy-loading-images/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -7,10 +8,12 @@ module.exports = {
     app: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/index.html'),
