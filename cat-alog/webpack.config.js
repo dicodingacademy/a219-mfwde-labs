a1c7b0bd96d6147272349487c68bc1bc2e75f3de
Nur Rizki Adi Prasetyo <nrizki@dicoding.com>
2023-10-06 07:23:12 +0700

    Update project version
    Add clean webpack plugin also

--- cat-alog/webpack.config.js
+++ cat-alog/webpack.config.js
@@ -1,17 +1,18 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
-const ImageminMozjpeg = require("imagemin-mozjpeg");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    app: path.resolve(__dirname, "src/index.js"),
+    app: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
@@ -19,31 +20,41 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
         ],
       },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "src/index.html"),
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/images/"),
-          to: path.resolve(__dirname, "dist/images/"),
+          from: path.resolve(__dirname, 'src/images/'),
+          to: path.resolve(__dirname, 'dist/images/'),
         },
       ],
     }),
     new ImageminWebpackPlugin({
+      /**
+       * Full documentations:
+       * https://github.com/Klathmon/imagemin-webpack-plugin#api
+       */
       plugins: [
         ImageminMozjpeg({
+          /**
+           * Full documentations:
+           * https://github.com/imagemin/imagemin-mozjpeg#api
+           */
           quality: 50,
           progressive: true,
         }),
