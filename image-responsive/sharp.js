88bab130a2d73f0646d279ac3486cab3ac9ac500
Nur Rizki Adi Prasetyo <nrizki@dicoding.com>
2023-10-06 07:32:38 +0700

    Update libraries version

--- image-responsive/sharp.js
+++ image-responsive/sharp.js
@@ -9,21 +9,29 @@ if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination);
 }
 
-fs.readdirSync(target)
-    .forEach(image => {
-      // mengubah ukuran gambar dengan lebar 800px, dengan prefix -large.jpg
-      sharp(`${target}/${image}`)
-          .resize(800)
-          .toFile(path.resolve(
-              __dirname,
-              `${destination}/${image.split('.').slice(0, -1).join('.')}-large.jpg`),
-          );
+/**
+ * Full documentations:
+ * https://sharp.pixelplumbing.com/api-constructor
+ * https://github.com/lovell/sharp#documentation
+ */
+fs.readdirSync(target).forEach((image) => {
+  // mengubah ukuran gambar dengan lebar 800px, dengan prefix -large.jpg
+  sharp(`${target}/${image}`)
+    .resize(800)
+    .toFile(
+      path.resolve(
+        __dirname,
+        `${destination}/${image.split('.').slice(0, -1).join('.')}-large.jpg`,
+      ),
+    );
 
-      // mengubah ukuran gambar dengan lebar 480px, dengan prefix -small.jpg
-      sharp(`${target}/${image}`)
-          .resize(480)
-          .toFile(path.resolve(
-              __dirname,
-              `${destination}/${image.split('.').slice(0, -1).join('.')}-small.jpg`),
-          );
-    });
+  // mengubah ukuran gambar dengan lebar 480px, dengan prefix -small.jpg
+  sharp(`${target}/${image}`)
+    .resize(480)
+    .toFile(
+      path.resolve(
+        __dirname,
+        `${destination}/${image.split('.').slice(0, -1).join('.')}-small.jpg`,
+      ),
+    );
+});
--- image-responsive/webpack.config.js
+++ image-responsive/webpack.config.js
@@ -1,15 +1,16 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    app: path.resolve(__dirname, "src/index.js"),
+    app: path.resolve(__dirname, 'src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
@@ -17,28 +18,30 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: "style-loader",
+            loader: 'style-loader',
           },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
           },
         ],
       },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "src/templates/index.html"),
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/templates/index.html'),
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src/public"),
-          to: path.resolve(__dirname, "dist"),
+          from: path.resolve(__dirname, 'src/public'),
+          to: path.resolve(__dirname, 'dist'),
           globOptions: {
-            // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
-            ignore: ["**/images/**"],
+            // CopyWebpackPlugin mengabaikan seluruh berkas yang berada dalam folder `images`
+            ignore: ['**/images/**'],
           },
         },
       ],
